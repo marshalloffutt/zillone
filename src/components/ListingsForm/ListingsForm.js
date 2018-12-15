b4d7f226ca77aacad24d4f62b1c6d716d1ee37ef
Marshall Offutt <marshalloffutt@gmail.com>
2018-12-15 09:27:54 -0600

    Adds sections

---
+++ src/components/ListingsForm/ListingsForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ListingsForm.scss';
+
+class ListingsForm extends React.Component {
+  render() {
+    return (
+      <div className="listingsForm col">
+        <h2>Listing Form</h2>
+      </div>
+    );
+  }
+}
+
+export default ListingsForm;
